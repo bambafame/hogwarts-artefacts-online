f3f51f65e0b188a9171a05d9000c374f051c63af
bambafame <105991062+bambafame@users.noreply.github.com>
2025-07-31 14:45:25 -0500

    Iniatal commit

---
+++ src/main/java/miu/edu/HogwartsArtefactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package miu.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtefactsOnlineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HogwartsArtefactsOnlineApplication.class, args);
+  }
+
+}
---
+++ src/test/java/miu/edu/HogwartsArtefactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtefactsOnlineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
